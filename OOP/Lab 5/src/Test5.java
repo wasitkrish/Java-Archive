b7048a3b4fd6f0211db64153c4a826bbe2f6e590
wasitkrish <singhkrish.main.acc@gmail.com>
2026-02-10 21:39:40 +0530

    Changes to test 5 and 6

--- OOP/Lab 5/src/Test5.java
+++ OOP/Lab 5/src/Test5.java
@@ -17,3 +17,17 @@ public class Test5{
         e1.setId(20);
     }
 }
+
+
+/** This program is based on comparision of Constructor vs Methods. In the program a constructor Employee is created which takes i as parameter and assigns to id;
+ * We also have a method setId that does the same thing as constructor;
+ * The main difference between these two are the constructor is called on object creation but method needs to be called explicitly to perform operations inside the method.
+ * Thats what the obove program show us.
+ *
+ * This program demonstrates the difference between a constructor and a method in Java.
+ * The class Employee contains a parameterized constructor that takes an integer value and initializes the instance variable id. This constructor is automatically invoked when an object of the class is created.
+ * The class also contains a method setId(), which performs a similar task of assigning a value to id, but it must be called explicitly using the object reference.
+ * In the main method, when the object e1 is created, the constructor is called automatically and initializes id with the value 10.
+ * Later, the method setId() is explicitly called to change the value of id to 20.
+ * This program shows that constructors are used for object initialization at the time of creation, whereas methods are used to perform operations after the object has been created.
+ */
--- OOP/Lab 5/src/Test6.java
+++ OOP/Lab 5/src/Test6.java
@@ -11,3 +11,10 @@ public class Test6{
         Counter c3 = new Counter();
     }
 }
+
+
+/** This program demonstrates that a constructor is called each time an object of a class is created.
+ The class Counter contains a default constructor that prints a message when an object is created.
+ In the main method, three objects (c1, c2, and c3) are created, so the constructor is invoked three times.
+ This proves that constructors are executed automatically during object creation.
+ */
