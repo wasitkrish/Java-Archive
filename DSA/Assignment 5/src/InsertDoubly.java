a2508c41acc39397bce90bd102ebc71d8cbce72e
wasitkrish <singhkrish.main.acc@gmail.com>
2026-02-22 00:02:42 +0530

    Made relevant changes in InsertDoubly.java, added conditions for error cases

--- DSA/Assignment 5/src/InsertDoubly.java
+++ DSA/Assignment 5/src/InsertDoubly.java
@@ -12,24 +12,35 @@ public class InsertDoubly {
     }
     static void insertDoubly(Node head,int k, Node newnode){
         Node temp=null;
-        for(int i=1;i<k;i++){
-            temp=head;
-            head=head.next;
+        Node traverse=head;
+        int count=0;
+        while(traverse!=null){
+            traverse=traverse.next;
+            count++;
         }
-        if(head.next==null){
-            newnode.prev=head;
-            head.next=newnode;
-            newnode.next=null;
-        }
-        else if(head.prev==null){
-            newnode.next=head;
-            head.prev=newnode;
+        if(k>count){
+            System.out.println("Position exceeds Node count in Linked List");
+            System.exit(1);
         }
         else {
-            newnode.next = head;
-            newnode.prev = temp;
-            temp.next = newnode;
-            head.prev = newnode;
+            for (int i = 1; i < k; i++) {
+                temp = head;
+                head = head.next;
+            }
+            if (head.next == null) {
+                newnode.prev = head;
+                head.next = newnode;
+                newnode.next = null;
+            } else if (head.prev == null) {
+                newnode.next = head;
+                head.prev = newnode;
+                newnode.prev = null;
+            } else {
+                newnode.next = head;
+                newnode.prev = temp;
+                temp.next = newnode;
+                head.prev = newnode;
+            }
         }
     }
     static void traverse(Node head){
@@ -49,7 +60,7 @@ public class InsertDoubly {
             v3.prev=v2; v3.next=v4;
             v4.prev=v3;
         Node newnode = new Node(25);
-            System.out.println("Enter position to enter :");
+            System.out.print("Enter position to enter :");
             int k = sc.nextInt();
             insertDoubly(v1,k,newnode);
             if(k==1)
