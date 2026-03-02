3bbebf1018a4eb6c19030aebcfdd9d614d39eaa3
wasitkrish <singhkrish.main.acc@gmail.com>
2026-03-03 01:30:14 +0530

    sorting practice added bubble sort after selection sort

---
+++ DSA/Sorting/src/Bubble.java
@@ -0,0 +1,19 @@
+public class Bubble{
+    public static void main(String[] args){
+        int[] arr = {3,4,7,5,1,2,6,8,9,0};
+        int temp=0;
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<(n-i-1);j++){
+                if(arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
