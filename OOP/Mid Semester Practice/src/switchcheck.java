5c199d6779889d6e007307e6e371687146a42976
wasitkrish <singhkrish.main.acc@gmail.com>
2026-03-11 23:00:21 +0530

    checked switch logic

---
+++ OOP/Mid Semester Practice/src/switchcheck.java
@@ -0,0 +1,17 @@
+public class switchcheck{
+    public static void main(String[] args){
+        int a = 10;
+        switch(a){
+            case 1:
+                System.out.println("1 guys");
+                break;
+            case 2:
+                System.out.println("2 guys");
+                break;
+            case 10:
+                System.out.println("10 guys");
+            case 'A':
+                System.out.println("Character A");
+        }
+    }
+}
