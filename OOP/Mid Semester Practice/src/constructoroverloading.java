f567d90f0dddea5edd9d6d09e0e0b4a8524897f7
wasitkrish <singhkrish.main.acc@gmail.com>
2026-03-11 23:39:37 +0530

    constructor overloading

---
+++ OOP/Mid Semester Practice/src/constructoroverloading.java
@@ -0,0 +1,28 @@
+class con{
+    private int first;
+    private int second;
+    private String name;
+    public con(int a, int b, String n){
+        this.first=a;
+        this.second=b;
+        this.name=n;
+    }
+    public con(int a, int b){
+        this.first=a;
+        this.second=b;
+        this.name="Krish";
+    }
+    public void display(){
+        System.out.println(first+" "+second+" and "+name);
+    }
+}
+public class constructoroverloading{
+    public static void main(String[] args){
+        con a = new con(2,2,"salan");
+        con b = new con(4,4,"niranjan");
+        con c = new con(5,5);
+        a.display();
+        b.display();
+        c.display();
+    }
+}
