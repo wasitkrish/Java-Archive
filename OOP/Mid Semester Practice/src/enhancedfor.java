756c8fbc41be391a7f0fa207898cbf4eaea5edcb
wasitkrish <singhkrish.main.acc@gmail.com>
2026-03-11 23:07:44 +0530

    analyzed enhanced for and its usage

---
+++ OOP/Mid Semester Practice/src/enhancedfor.java
@@ -0,0 +1,8 @@
+public class enhancedfor{
+    public static void main(String[] forarghs){
+        int[] arr = {1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9};
+        for(int m:arr){
+            System.out.print(m+" ");
+        }
+    }
+}
