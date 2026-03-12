4f18a4e7efa7e55a9761a07048f0ac5b31163385
wasitkrish <singhkrish.main.acc@gmail.com>
2026-03-13 02:05:28 +0530

    Mid Semester DSA practice - Traversing singlyLinkedList updated

--- DSA/midsem practice/src/TraversingLinkedList.java
+++ DSA/midsem practice/src/TraversingLinkedList.java
@@ -9,15 +9,18 @@ public class TraversingLinkedList{
     }
     static void traverse(Node head){
         Node temp=head;
+        int count=0;
         if(head.next==null){
             return;
         }
         else{
             while(head!=null){
                 System.out.println(head.name);
+                count++;
                 head=head.next;
             }
         }
+        System.out.println("Number of Students: "+count);
     }
     public static void main(String[] args){
         Node a = new Node("Aditi");
