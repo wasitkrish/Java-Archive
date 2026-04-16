1cfa69b5efe9ac92f215438bd4357fc3590027ca
wasitkrish <singhkrish.main.acc@gmail.com>
2026-04-17 01:28:14 +0530

    Sorting Questions of assignment 6 1 and 2 completed

--- DSA/Assignment 6/src/ProductPriceSelectionSort.java
+++ DSA/Assignment 6/src/ProductPriceSelectionSort.java
@@ -1,7 +1,27 @@
-public class ProductPriceSelectionSort{
-    public static void main(String[] args){
+public class ProductPriceSelectionSort {
+    public static void main(String[] args) {
         Test t = new Test();
-        int[][] data=t.a;
-
+        int[][] data = t.a;
+        for (int all = 0; all < data.length; all++) {
+            int n= data[all].length;
+            for (int i = 0; i < n; i++) {
+                int minindex = i;
+                for (int j = i + 1; j < n; j++) {
+                    if(data[all][j]<data[all][minindex]){
+                        minindex=j;
+                    }
+                }
+                if(minindex!=i){
+                    int temp= data[all][minindex];
+                    data[all][minindex]=data[all][i];
+                    data[all][i]=temp;
+                }
+            }
+            System.out.println("Sorted Array "+all+" is: ");
+            for(int i=0;i<n;i++){
+                System.out.print(data[all][i]+" ");
+            }
+            System.out.println();
+        }
     }
 }
